64f44969630d0b6711a10ee62df9322938ec8e38
Is-aak <62933096+Is-aak@users.noreply.github.com>
2020-04-23 16:01:45 +0200

    +Methode Division

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -14,6 +14,7 @@ namespace Minirechner
             int ErgebnissAd;
             int ErgebnissSub;
             int ErgebnissMul;
+            int ErgebnissDiv;
 
             Console.Write("Zahl 1 = ");
             Zahl1 = Convert.ToInt32(Console.ReadLine());
@@ -22,18 +23,21 @@ namespace Minirechner
             Zahl2 = Convert.ToInt32(Console.ReadLine());
           
             ErgebnissAd = Addieren(Zahl1, Zahl2);
-
             Console.Write("Ergebniss Addition = ");
             Console.WriteLine(ErgebnissAd);
 
             ErgebnissSub = Subtrahieren(Zahl1, Zahl2);
-
             Console.Write("Ergebniss Subtraktion = ");
             Console.WriteLine(ErgebnissSub);
 
             ErgebnissMul = Multipliezieren(Zahl1, Zahl2);
             Console.Write("Ergebniss Multiplikation");
             Console.WriteLine(ErgebnissMul);
+
+            ErgebnissDiv = Division(Zahl1, Zahl2);
+            Console.Write("Ergebniss Division");
+            Console.WriteLine(ErgebnissDiv);
+
 
             Console.ReadKey();
 
@@ -51,6 +55,11 @@ namespace Minirechner
         static int Multipliezieren(int a, int b)
         {
             int Ergebniss = a * b;
+            return Ergebniss;
+        }
+        static int Division(int a, int b)
+        {
+            int Ergebniss = a / b;
             return Ergebniss;
         }
     }
